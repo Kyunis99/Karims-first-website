04cac9d7919b3ae3c146632ad7aeea36f5ae3c13
Kyunis99 <Yunis_karim@columbusstate.edu>
2023-03-20 18:53:41 -0400

    added jobitem

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+JOBS =[
+    {
+        'id' : 1,
+        'title' : 'Software Engineer',
+        'location' : 'San Francisco, CA',
+        'salary' : '$100,000'
+    },
+    {
+        'id' : 2,
+        'title' : 'Data Scientist',
+        'location' : 'Seattle, WA',
+        'salary' : '$90,000'
+    },
+    {
+        'id' : 3,
+        'title' : 'Front End Developer',
+        'location' : 'Remote',
+        'salary' : '$95,000'
+    },
+    {
+        'id' : 4,
+        'title' : 'Back End Developer',
+        'location' : 'Dallas, TX',
+    }
+]
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", jobs = JOBS,
+                           person_name = "Karim's")
 
 
 if __name__ == "__main__":
